113f052ac6d811a3568ce3cd8ff8d4182f8f649a
darlingPc <rombk2711@gmail.com>
2023-10-01 00:04:52 +0700

    add auth screen

--- app/src/main/java/com/shadowshiftstudio/jobcentre/MainActivity.kt
+++ app/src/main/java/com/shadowshiftstudio/jobcentre/MainActivity.kt
@@ -3,27 +3,93 @@ package com.shadowshiftstudio.jobcentre
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.shadowshiftstudio.jobcentre.app_view.authentication.view.AuthorizationScreen
 import com.shadowshiftstudio.jobcentre.app_view.theme.JobCentreTheme
+import com.shadowshiftstudio.jobcentre.app_view.theme.md_theme_dark_background
+import com.shadowshiftstudio.jobcentre.view.employer.view.bottomnav.ConstantsEmployer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JobCentreTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+            JobCentreTheme(dynamicColor = false, darkTheme = true) {
+                val navController = rememberNavController()
+
+                Surface {
+                    AuthorizationScreen(navController = navController) {
+
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun NavHostContainerEmployer(
+    navHostController: NavHostController,
+    paddingValues: PaddingValues,
+) {
+    NavHost(
+        navController = navHostController,
+        startDestination = "home",
+        modifier = Modifier.padding(paddingValues = paddingValues),
+
+        builder = {
+            composable("home") {
+
+            }
+        }
+    )
+}
+
+@Composable
+fun BottomNavigationBarEmployer(navHostController: NavHostController) {
+    NavigationBar(
+        containerColor = md_theme_dark_background
+    ) {
+        val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        ConstantsEmployer.BottomNavItems.forEach { navItem ->
+
+            NavigationBarItem(
+                selected = currentRoute == navItem.route,
+
+                onClick = {
+                    navHostController.navigate(navItem.route)
+                },
+
+                icon = {
+                    Icon(imageVector = navItem.icon, contentDescription = navItem.title)
+                },
+
+                label = {
+                    Text(text = navItem.title)
+                },
+
+                alwaysShowLabel = true
+            )
         }
     }
 }
--- app/src/main/java/com/shadowshiftstudio/jobcentre/app_view/authentication/view_model/AuthorizationViewModel.kt
+++ app/src/main/java/com/shadowshiftstudio/jobcentre/app_view/authentication/view_model/AuthorizationViewModel.kt
@@ -1,8 +1,16 @@
 package com.shadowshiftstudio.jobcentre.app_view.authentication.view_model
 
 import android.content.Context
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AuthorizationViewModel(private val context: Context): ViewModel() {
+    var login: MutableState<String> = mutableStateOf("")
+    var password: MutableState<String> = mutableStateOf("")
+
+    val loginStatusLiveData: MutableLiveData<Boolean> = MutableLiveData()
+
 
 }
